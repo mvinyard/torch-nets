535e1770f2589668df9e702a347d85e51cf25b8e
mvinyard <mvinyard@broadinstitute.org>
2022-11-11 13:59:30 -0700

    add re-structured base layer unit

--- torch_composer/_core/_base/_layer.py
+++ torch_composer/_core/_base/_layer.py
@@ -6,61 +6,105 @@ __email__ = ", ".join(["vinyard@g.harvard.edu"])
 
 
 # -- import packages: --------------------------------------------------------------------
+from abc import ABC
+from collections import OrderedDict
 import torch
 
 
-# -- import local dependencies: ----------------------------------------------------------
-from ._abstract_base_models import BaseLayer
+# -- Base Layer module: ------------------------------------------------------------------
+class Layer(ABC):
+    __name__ = ""
 
-
-# -- Layer module: -----------------------------------------------------------------------
-class Layer(BaseLayer):
     def __init__(
         self,
-        name=None,
-        nodes_m=50,
-        nodes_n=1,
-        activation_function=None,
-        dropout=0,
+        in_features,
+        out_features,
+        activation=None,
         bias=True,
-        layer_type=None
+        dropout=0,
+        name: str = None,
     ):
-        super(Layer).__init__()
-        self.__name__(name)
-        if activation_function:
-            self.__add_activation__(activation_function)
-        self.__add_layer__(nodes_m, nodes_n, bias, layer_type)
-        if dropout:
-            self.__add_dropout__(dropout)
-            
-    def __name__(self, name):
-        if name:
-            setattr(self, "_name", "_{}".format(name))
-        else:
-            setattr(self, "_name", "")
+        """
+        
+        Parameters:
+        -----------
+        in_features
+        
+        out_features
+        
+        activation
+        
+        bias
+        
+        dropout
+        
+        name
+        
+        Returns:
+        --------
+        
+        
+        Notes:
+        ------
+        (1) General flow assumed is: Linear -> Dropout -> Activation
+        """
+        
+        self.__parse_kwargs__(name, locals())
 
-    def __add_activation__(self, activation_function):
-        setattr(
-            self,
-            "activation{}".format(self._name),
-            activation_function,
+    # --- utilities: ---------------------------------------------------------------------
+    def __configure_activation__(self, func):
+
+        if isinstance(func, str):
+            return getattr(torch.nn, func)()
+        if isinstance(func, torch.nn.Module):
+            return func
+        if isinstance(func(), torch.nn.Module):
+            return func()
+
+        print(
+            "Must pass torch.nn.<function> or a string that fetches a torch.nn.<function>"
         )
 
-    def __add_layer__(self, nodes_m, nodes_n, bias, layer_type=""):
-        if layer_type:
-            layer_name = "{}{}".format(layer_type, self._name)
-        else:
-            layer_name = "{}".format(self._name)
-        setattr(self, layer_name, torch.nn.Linear(nodes_m, nodes_n, bias=bias))
+    def __parse_kwargs__(self, name, kwargs, ignore=["self", "name"]):
 
-    def __add_dropout__(self, dropout):
-        setattr(self, "dropout{}".format(self._name), torch.nn.Dropout(p=dropout))
+        setattr(self, "__name__", name)
 
+        self._kwargs = {}
+        for k, v in kwargs.items():
+            if k and (not k in ignore):
+                self._kwargs[k] = v
+                setattr(self, "_{}".format(k), v)
+
+    # -- core properties: ----------------------------------------------------------------
+    @property
+    def linear(self):
+        return torch.nn.Linear(
+            in_features=self._in_features,
+            out_features=self._out_features,
+            bias=self._bias,
+        )
+
+    @property
+    def dropout(self):
+        if self._dropout:
+            return torch.nn.Dropout(self._dropout)
+
+    @property
+    def activation(self):
+        if self._activation:
+            return self.__configure_activation__(self._activation)
+
+    # -- called: -------------------------------------------------------------------------
     def __collect_attributes__(self):
 
-        attributes = [attr for attr in self.__dir__() if not attr.startswith("_")]
+        attributes = [i for i in layer.__dir__() if not i.startswith("_")]
         for attr in attributes:
-            yield attr, getattr(self, attr),
+            if not getattr(self, attr) is None:
+                if self.__name__:
+                    attr_name = "_".join([self.__name__, attr])
+                else:
+                    attr_name = attr
+                yield (attr_name, getattr(self, attr))
 
     def __call__(self) -> dict:
-        return dict(self.__collect_attributes__())
+        return torch.nn.Sequential(OrderedDict(self.__collect_attributes__()))
