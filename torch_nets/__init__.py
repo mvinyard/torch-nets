4cafc6d3f530b6daee60827ffb9e58c7ed411703
mvinyard <mvinyard.ai@gmail.com>
2023-04-12 17:39:13 +0000

    update version 0.0.3

--- torch_nets/__init__.py
+++ torch_nets/__init__.py
@@ -3,7 +3,7 @@ __module_name__ = "__init__.py"
 __doc__ = """Main API __init__.py module."""
 __author__ = ", ".join(["Michael E. Vinyard"])
 __email__ = ", ".join(["vinyard@g.harvard.edu",])
-__version__ = "0.0.3rc2"
+__version__ = "0.0.3"
 
 
 # -- import network modules: -------------------------------------------------------------
