8a46a7cc0d21ea94a0499f71761b9d809025560e
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2023-04-12 11:07:54 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 
 setuptools.setup(
     name="torch-nets",
-    version="0.0.3rc0",
+    version="0.0.3rc1",
     python_requires=">3.7.0",
     author="Michael E. Vinyard - Harvard University - Broad Institute of MIT and Harvard - Massachussetts General Hospital",
     author_email="mvinyard@broadinstitute.org",
@@ -17,8 +17,9 @@ setuptools.setup(
     description="API to compose PyTorch neural networks on the fly.",
     packages=setuptools.find_packages(),
     install_requires=[
-        "torch>=1.12",
+        "torch>=2.0.0",
         "ABCParse>=0.0.3",
+        "vinplots>=0.0.75",
     ],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
