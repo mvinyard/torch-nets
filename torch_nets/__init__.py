983d8c6147b32ac7e067714c7ad3eb3fb129b2ab
Michael Vinyard <mvinyard.ai@gmail.com>
2025-02-28 12:52:15 -0700

    Update version

--- torch_nets/__init__.py
+++ torch_nets/__init__.py
@@ -4,7 +4,7 @@ __doc__ = """Main torch-nets API __init__.py module."""
 __author__ = "Michael E. Vinyard"
 __email__ = "mvinyard.ai@gmail.com"
 
-
+from .__version__ import __version__
 # -- import network modules: -------------------------------------------------------------
 from ._torch_net import TorchNet
 from ._encoder import Encoder
@@ -16,4 +16,12 @@ from ._augmented_torch_net import AugmentedTorchNet
 from . import core
 from . import tools as tl
 
-__all__ = ["TorchNet", "Encoder", "Decoder", "AugmentedTorchNet", "core", "tl"]
+__all__ = [
+    "TorchNet",
+    "Encoder",
+    "Decoder",
+    "AugmentedTorchNet",
+    "core",
+    "tl",
+    "__version__",
+]
